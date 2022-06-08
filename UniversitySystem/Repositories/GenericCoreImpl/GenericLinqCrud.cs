aa4784e171842f7770521886ae7c8252b698ea65
haideratish2689@gmail.com <haideratish2689@gmail.com>
2022-06-09 01:30:40 +0500

    Third commit

---
+++ UniversitySystem/Repositories/GenericCoreImpl/GenericLinqCrud.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversitySystem.Repositories.GenericCoreImpl
+{
+    internal class GenericLinqCrud
+    {
+    }
+}
